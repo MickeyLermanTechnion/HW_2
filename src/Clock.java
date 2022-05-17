8e70fe83362d40d08acae6e84d6fb7c50239ae31
Shachart1 <shachart@campus.technion.ac.il>
2022-05-17 16:00:41 +0300

    SHLUMPI

--- src/Clock.java
+++ src/Clock.java
@@ -53,7 +53,7 @@ public class Clock {
     public boolean equals(Object o){
         if(this == null) {return false;}
         if (this == o) {return true;}
-        if(o instanceof Clock && this.toString().length()==o.toString().length()){ //TODO: change to hash hash hash
+        if(o instanceof Clock && this.toString().length()==o.toString().length()){
             Clock other = (Clock)o;
             if(this.hours==other.getHours() && this.minutes==other.getMinutes() ){
                 return true;
