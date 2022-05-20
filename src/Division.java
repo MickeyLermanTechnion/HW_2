ecb73f312ffafdff7f9bf0aa5169e9877b9c5f7d
MickeyLermanTechnion <102416767+MickeyLermanTechnion@users.noreply.github.com>
2022-05-20 11:58:48 +0300

    whoooho

---
+++ src/Division.java
@@ -0,0 +1,12 @@
+public class Division  extends Expression{
+    public Division(Expression x, Expression y){
+        super(x,y);
+    }
+
+    public String toString(){
+        return super.toString(this.expressions, '/');
+    }
+    public double evaluate(){
+        return this.expressions[0].evaluate() - this.expressions[1].evaluate();
+    }
+}
